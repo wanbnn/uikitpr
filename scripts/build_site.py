b05ff4376c57c37e189d70d8538aa487d6b4bb7a
wanbnn <gcncomercial@gmail.com>
2026-07-27 12:36:17 -0300

    style: use Inter Tight on documentation site

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -632,7 +632,7 @@ def Document():
             h(
                 "link",
                 {
-                    "href": "https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=Manrope:wght@400;500;600;700;800&display=swap",
+                    "href": "https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=Inter+Tight:wght@400;500;600;700;800&display=swap",
                     "rel": "stylesheet",
                 },
             ),
